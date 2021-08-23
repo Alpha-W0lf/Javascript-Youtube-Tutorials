700105ef1ff1f2c8f37e2a1c85ed27ab2abc9e72
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-23 11:04:49 -0500

    Update app.js

---
+++ Learn JavaScript - Full Course for Beginners/app.js
@@ -0,0 +1,12 @@
+// This is how you comment in JS
+
+var number = 5; // This is an in-line comment
+
+/* Can also do multiline comments using \/\* and \*\/
+
+escape characters using \
+
+afswg
+awgawd
+getComputedStyleFRw
+f */
