8bcb4fbbd7bf6a0723ef77b293a8df997e9ce701
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-23 17:30:44 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -286,3 +286,23 @@ function functionWithArgs(a, b) {
 }
 
 functionWithArgs(10, 5);
+
+var myGlobal = 10;
+
+function fun1() {
+    var oopsGlobal = 5;
+}
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+
+fun1();
+fun2();
