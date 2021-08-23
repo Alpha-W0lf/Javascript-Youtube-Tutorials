38e9cad9a844ae942b671780a1ef51d0a3a83a2b
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-23 17:27:56 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -275,3 +275,14 @@ function reusableFunction() {
 }
 
 reusableFunction();
+
+function ourFunctionWithArgs(a, b) {
+    console.log(a - b);
+}
+ourFunctionWithArgs(10, 5);
+
+function functionWithArgs(a, b) {
+    console.log(a + b);
+}
+
+functionWithArgs(10, 5);
