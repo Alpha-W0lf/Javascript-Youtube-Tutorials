36e7468b19ba82baf5087540a6c2698274e5324a
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-23 13:44:18 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -240,3 +240,7 @@ ourArray[1] = 45; // arrays are NOT immutable. we can change the second item in 
 
 var myArray = [18,64,99];
 myArray[2] = 7;
+
+var arrayOfArrays = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+var myData = myArray[0][0];
