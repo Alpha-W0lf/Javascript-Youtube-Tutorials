db016a3b5dbb7501a4229545c8e9dee803f31fec
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-24 13:39:35 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -378,16 +378,19 @@
 
 // console.log(welcomeToBooleans());
 
-function ourTrueOrFalse(isItTrue) {
-    if (isItTrue) {
-        return "Yes, it's true";
-    }
-    return "No, it's false";
-}
+// function ourTrueOrFalse(isItTrue) {
+//     if (isItTrue) {
+//         return "Yes, it's true";
+//     }
+//     return "No, it's false";
+// }
 
-function trueOrFalse(wasThatTrue) {
-    
+// function trueOrFalse(wasThatTrue) {
+//     if (wasThatTrue) {
+//         return "Yes, that was true";
+//     }
+//     return "No, that was false";
+// }
 
-}
+// console.log(trueOrFalse(true));
 
-trueOrFalse();
