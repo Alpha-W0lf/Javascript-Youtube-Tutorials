01e4522170a3d1fbbe7e104897bc43e8d9f340d6
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-25 13:12:35 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -610,24 +610,25 @@ par + 1     "Bogey"
 par + 2     "Double Bogey"
 >= par + 3  "Go Home!"
 */
-var names = ["Hole-In-One!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"]
-function golfScore(par, strokes) {
-    if (strokes <= 0) {
-        return "Error. Strokes cannot be less than 1!";
-    } else if (strokes == 1) {
-        return names[0];
-    } else if (strokes <= (par - 2)) {
-        return (names[1]);
-    } else if (strokes == (par - 1)) {
-        return (names[2]);
-    } else if (strokes == par) {
-        return (names[3]);
-    } else if (strokes == (par + 1)) {
-        return (names[4]);
-    } else if (strokes == (par + 2)) {
-        return (names[5]);
-    } else {
-        return (names[6]);
-    }
-}
-console.log(golfScore(5, 0));
+// var names = ["Hole-In-One!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"]
+// function golfScore(par, strokes) {
+//     if (strokes <= 0) {
+//         return "Error. Strokes cannot be less than 1!";
+//     } else if (strokes == 1) {
+//         return names[0];
+//     } else if (strokes <= (par - 2)) {
+//         return (names[1]);
+//     } else if (strokes == (par - 1)) {
+//         return (names[2]);
+//     } else if (strokes == par) {
+//         return (names[3]);
+//     } else if (strokes == (par + 1)) {
+//         return (names[4]);
+//     } else if (strokes == (par + 2)) {
+//         return (names[5]);
+//     } else {
+//         return (names[6]);
+//     }
+// }
+// console.log(golfScore(5, 0));
+
