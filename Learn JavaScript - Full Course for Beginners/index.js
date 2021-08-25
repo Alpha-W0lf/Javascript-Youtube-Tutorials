65595107047e71cfba7ebbac412a5ca4d4b468d3
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-25 12:55:56 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -567,13 +567,35 @@
 // }
 // console.log(testElif(87));
 
-function orderMyLogic(val) {
-    if (val < 5) {
-        return "Less than 5";
-    } else if (val < 10) {
-        return "Less than 10";
+// function orderMyLogic(val) {
+//     if (val < 5) {
+//         return "Less than 5";
+//     } else if (val < 10) {
+//         return "Less than 10";
+//     } else {
+//         return "Greater than or equal to 10";
+//     }
+// }
+// console.log(orderMyLogic(3));
+
+/* Write chained if/else if statements to fulfill the following conditions:
+num < 5 : return "Tiny"
+num < 10 : return "Small"
+num < 15 : return "Medium"
+num < 20 : return "Large"
+num >= 20 : return "Huge"
+*/
+function testSize(num) {
+    if (num < 5) {
+        return "Tiny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium";
+    } else if (num < 20) {
+        return "Large";
     } else {
-        return "Greater than or equal to 10";
+        return "Huge";
     }
 }
-console.log(orderMyLogic(3));
+console.log(testSize(21));
