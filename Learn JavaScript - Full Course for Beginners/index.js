eae6a767689aa45907b82491240f52a0dea07b33
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-31 14:26:26 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -935,3 +935,18 @@ par + 2     "Double Bogey"
 
 // console.log(phoneticLookup("foxtrot"));
 
+var myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+    if (myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp];
+    } else {
+        return "Not Found";
+    }
+};
+
+console.log(checkObj("hello"));
