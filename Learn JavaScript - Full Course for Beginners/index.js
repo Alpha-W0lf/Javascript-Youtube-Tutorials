8c0bbf692c8282bb5766fc787c975160197e8503
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-31 11:53:48 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -734,8 +734,19 @@ par + 2     "Double Bogey"
 // }
 // chainToSwitch(1);
 
-function isLess(a, b) {
-    return a < b;
+// function isLess(a, b) {
+//     return a < b;
+// }
+
+// console.log(isLess(10, 1));
+
+function abTest(a, b) {
+    if (a < 0 || b < 0) {
+        return undefined;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-console.log(isLess(10, 1));
+abTest(2, 2);
+console.log(abTest(2, 2));
