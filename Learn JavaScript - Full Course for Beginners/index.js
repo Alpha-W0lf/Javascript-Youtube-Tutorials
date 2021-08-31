66c57009c64865bc781434ecc65ac61bb7e4b5e7
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-31 11:49:04 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -708,3 +708,29 @@ par + 2     "Double Bogey"
 // }
 // console.log(sequentialSizes(81));
 
+function chainToSwitch(val) {
+    var answer = "";
+
+    switch(val) {
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much!";
+            break;
+        case 7:
+            answer = "Ate Nine";
+            break;
+        default:
+            answer = "Default value";
+    }
+    return console.log(answer);
+}
+chainToSwitch(1);
+
