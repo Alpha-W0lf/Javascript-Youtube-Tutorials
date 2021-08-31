ea961aa9095c0ccb5bab2a3ebbc7f6ba4a0c444c
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-31 13:55:41 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -828,3 +828,21 @@ par + 2     "Double Bogey"
 // var player = testObj[playerNumber];
 // console.log(player);
 
+// var ourDog = {
+//     "name": "Camper",
+//     "legs": 4,
+//     "tails": 1,
+//     "friends": ["everything!"]
+// };
+
+// ourDog.name = "Happy Camper";
+
+// var myDog = {
+//     "name": "Coder",
+//     "legs": 4,
+//     "tails": 1,
+//     "friends": ["freeCodeCamp Campers"]
+// };
+
+// myDog.name = "Happy Coder";
+// console.log(myDog.name);
