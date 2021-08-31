351c64df0c6a45c36c018e572e0677852cc2ad90
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-31 11:59:36 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -740,13 +740,44 @@ par + 2     "Double Bogey"
 
 // console.log(isLess(10, 1));
 
-function abTest(a, b) {
-    if (a < 0 || b < 0) {
-        return undefined;
-    }
+// function abTest(a, b) {
+//     if (a < 0 || b < 0) {
+//         return undefined;
+//     }
 
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
-}
+//     return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+// }
 
-abTest(2, 2);
-console.log(abTest(2, 2));
+// abTest(2, 2);
+// console.log(abTest(2, 2));
+
+// var count = 0;
+
+// function cc(card) {
+//     switch(card) {
+//         case 2:
+//         case 3:
+//         case 4:
+//         case 5:
+//         case 6:
+//             count++;
+//             break;
+//         case 10:
+//         case "J":
+//         case "Q":
+//         case "K":
+//         case "A":
+//             count--;
+//             break;
+
+//     }
+//     var holdBet = "Hold";
+//     if (count > 0) {
+//         holdBet = "Bet";
+//     }
+//     return count + " " + holdBet;
+// }
+
+// cc(2); cc("K"); cc(7); cc('K'); cc('A');
+// console.log(cc(4));
+
