a15527eb8d867f19d23b8ad05b48c62b2209f369
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-31 19:26:14 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -1010,37 +1010,45 @@ par + 2     "Double Bogey"
 // var secondTree = myPlants[1].list[1];
 // console.log(secondTree);
 
-var collection = {
-    "2548": {
-        "album": "Slippery When Wet",
-        "artist": "Bon Jovi",
-        "tracks": [
-            "Let It Rock",
-            "You Give Love a Bad Name"
-        ]
-    },
-    "2468": {
-        "album": "1999",
-        "artist": "Prince",
-        "tracks": [
-            "1999",
-            "Little Red Corvette"
-        ]
-    },
-    "1245": {
-        "artist": "Robert Palmer",
-        "tracks": []
-    },
-    "5439": {
-        "album": "ABBA Gold"
-    }
-};
+// var collection = {
+//     "2548": {
+//         "album": "Slippery When Wet",
+//         "artist": "Bon Jovi",
+//         "tracks": [
+//             "Let It Rock",
+//             "You Give Love a Bad Name"
+//         ]
+//     },
+//     "2468": {
+//         "album": "1999",
+//         "artist": "Prince",
+//         "tracks": [
+//             "1999",
+//             "Little Red Corvette"
+//         ]
+//     },
+//     "1245": {
+//         "artist": "Robert Palmer",
+//         "tracks": []
+//     },
+//     "5439": {
+//         "album": "ABBA Gold"
+//     }
+// };
 
-var collectionCopy = JSON.parse(JSON.stringify(collection));
+// var collectionCopy = JSON.parse(JSON.stringify(collection));
 
-function udateRecords(id, prop, value) {
-    
-    return collection;
-}
+// function updateRecords(id, prop, value) {
+//     if (value === "") {
+//         delete collection[id][prop]
+//     } else if (prop === "tracks") {
+//         collection[id][prop] = collection[id][prop] || [];
+//         collection[id][prop].push(value);
+//     } else {
+//         collection[id][prop] = value;
+//     }
 
-console.log(collection);
+//     return collection;
+// }
+// updateRecords(5439, "artist", "ABBA");
+// console.log(updateRecords(2468, "tracks", "TEST"));
