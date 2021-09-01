2c54cee09acda614076f873e5706b107b5fa24fe
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-31 19:15:15 -0500

    Update index.js

--- Learn JavaScript - Full Course for Beginners/index.js
+++ Learn JavaScript - Full Course for Beginners/index.js
@@ -1010,3 +1010,37 @@ par + 2     "Double Bogey"
 // var secondTree = myPlants[1].list[1];
 // console.log(secondTree);
 
+var collection = {
+    "2548": {
+        "album": "Slippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    "2468": {
+        "album": "1999",
+        "artist": "Prince",
+        "tracks": [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": []
+    },
+    "5439": {
+        "album": "ABBA Gold"
+    }
+};
+
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+function udateRecords(id, prop, value) {
+    
+    return collection;
+}
+
+console.log(collection);
