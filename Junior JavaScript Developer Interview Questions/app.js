b65b6df5cae12c6fbb0d4d908ea398542af39a55
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-09-03 21:30:53 -0500

    palindrome function

---
+++ Junior JavaScript Developer Interview Questions/app.js
@@ -0,0 +1,24 @@
+// Palindrome problem: Create a program that tells user if a word is a palindrome or not.
+
+function isPalindrome(given) {
+    let given_lower = given.toLowerCase();
+    let array = given_lower.split('');
+    let array_reverse = array.reverse();
+    reverse = array_reverse.join('');
+    console.log(reverse);
+    return given_lower === reverse;
+    // for (var i = 0; i < array.length; i++) {
+    //     if(array[i] !== array_reverse[i]) {
+    //         return false;
+    //     ;} else {
+    //         return true;
+    // }
+    // }
+    // if (given_lower_array === given_lower_array_reverse) {
+    //     return palindrome = true;
+    // } else {
+    //     return palindrome = false;
+    // }
+};
+
+console.log(isPalindrome("malayalam"));
