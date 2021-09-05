1fcca69e0c4ad6ee2900fab3bb93888d39b93518
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-09-05 13:09:35 -0500

    adding increment button

--- What is State in Programming/state.js
+++ What is State in Programming/state.js
@@ -1,14 +1,35 @@
-let counter = 0;
+import React, { useState } from 'react';
+import '.styles.css';
 
-function incrementCounter() {
-    counter = counter + 1;
-    renderState();
+// let counter = 0;
+
+// function incrementCounter() {
+//     counter = counter + 1;
+//     renderState();
+// }
+
+// function renderState() {
+//     console.log(counter);
+// }
+
+// setInterval(function () {
+//     incrementCounter();
+// }, 2000);
+
+<h2>HELLO</h2>
+
+function App() {
+    const [counter, setCounter] = useState(0);
+
+    function incrementCounterHandler() {
+        setCounter((prevCounter) => prevCounter + 1);
+    }
+    return (
+        <main className='app'>
+            <p>{counter}</p>
+            <button onClick={incrementCounterHandler}>Increment</button>
+        </main>
+    );
 }
 
-function renderState() {
-    console.log(counter);
-}
-
-setInterval(function () {
-    incrementCounter();
-}, 2000);
+export default state;
