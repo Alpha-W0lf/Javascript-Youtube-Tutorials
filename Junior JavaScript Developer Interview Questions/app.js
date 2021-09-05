0c1fc67e134f6bc278153ea3b60a70080be1880d
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-09-04 21:52:18 -0500

    Update app.js

--- Junior JavaScript Developer Interview Questions/app.js
+++ Junior JavaScript Developer Interview Questions/app.js
@@ -28,18 +28,32 @@ console.log(isPalindrome("mAlayalam"));
 // Fibonacci problem: A Fibonacci sequence is an ordering of numbers where each number is the sum of the preceding two. For example, the first ten numbers of the Fibonacci sequence are: 0,1,1,2,3,5,8,13,21,34.
 // The Fibonacci challenege usually goes like this: write a function that returns the nth entry in the Fibonacci sequence, where n is a number you pass in as argument to the function.
 
+// function fibEntry(n) {
+//     let sequence = [0,1];
+//     // console.log(sequence);
+//     // let lastNum = sequence.slice(-1);
+//     // let secondLastNum = sequence.slice(-2);
+//     // console.log(lastNum + secondLastNum);
+//     for(let i = 2; i <= n; i++) {
+//         const prevNum1 = sequence[i - 1];
+//         const prevNum2 = sequence[i - 2];
+//         sequence.push(prevNum1 + prevNum2);
+//     }
+//     console.log(sequence[n]);
+// };
+
+// fibEntry(6);
+
 function fibEntry(n) {
-    let sequence = [0,1];
-    // console.log(sequence);
-    // let lastNum = sequence.slice(-1);
-    // let secondLastNum = sequence.slice(-2);
-    // console.log(lastNum + secondLastNum);
-    for(let i = 2; i <= n; i++) {
-        const prevNum1 = sequence[i - 1];
-        const prevNum2 = sequence[i - 2];
-        sequence.push(prevNum1 + prevNum2);
+    let prev = 0;
+    let curr = 1;
+    let index = 2;
+
+    while (index !== n) {
+        [prev, curr] = [curr, prev + curr];
+        index++;
     }
-    console.log(sequence[n]);
+    return curr;
 };
 
-fibEntry(5);
+console.log(fibEntry(6));
