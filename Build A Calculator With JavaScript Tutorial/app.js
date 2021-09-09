157d03be8cd744e1735f821a0f166b59b8a29c4d
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-09-08 20:32:12 -0500

    Update app.js

--- Build A Calculator With JavaScript Tutorial/app.js
+++ Build A Calculator With JavaScript Tutorial/app.js
@@ -16,7 +16,7 @@ class Calculator {
     }
 
     appendNumber(number) {
-
+        this.currentOperand = number
     }
 
     chooseOperation(operation) {
@@ -28,15 +28,24 @@ class Calculator {
     }
 
     updateDisplay() {
-
+        this.currentOperandTextElement.innerText = this.currentOperand
     }
 }
 
 
-const numberButtons = document.querySelectorAll('[data-number');
+const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
